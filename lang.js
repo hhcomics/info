9601bc1ee0a20c0489ef57a82605f269e99b1e6c
hhcomics <master.move456@gmail.com>
2025-12-27 19:12:34 +0100

    add new

---
+++ lang.js
@@ -0,0 +1,19 @@
+//ändra tillstånd
+function state(className,newState){    
+    let classLength = document.getElementsByClassName(className).length;
+    for(let i = 0;i<classLength;i++){
+        document.getElementsByClassName(className)[i].style.display=newState;
+    }
+}
+//engelska
+function eng(){
+    state("eng","block");
+    state("sve","none");
+}
+//svenska
+function sve(){    
+    state("eng","none");
+    state("sve","block");
+}
+//kör engelska per default
+window.onload=sve;
